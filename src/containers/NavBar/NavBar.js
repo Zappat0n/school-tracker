0ba42e48a125c37b6ea8f3540664cf306fd82756
Zappat0n <albarros1973@gmail.com>
2021-04-16 15:27:19 +0200

    Add navigation menu

--- src/containers/NavBar/NavBar.js
+++ src/containers/NavBar/NavBar.js
@@ -4,16 +4,37 @@ import './NavBar.scss';
 
 const NavBar = () => {
   const title = useSelector((state) => state.user.title);
+
+  const toggleMenu = (e) => {
+    e.preventDefault();
+    const content = document.querySelector('.menu-content');
+    if (content.classList.contains('d-none')) {
+      content.classList.remove('d-none');
+    } else {
+      content.classList.add('d-none');
+    }
+  };
+
   return (
-    <div className="nav-bar">
-      <div className="center" />
-      {title}
-      <div className="right">
-        <Link to="/user/signin" className="link">
-          <i className="fas fa-user" />
-        </Link>
+    <>
+      <div className="nav-bar">
+        <div className="left nav-col ta-left">
+          <i id="menu-toggle" role="none" className="fas fa-bars" onKeyDown={(e) => toggleMenu(e)} onClick={(e) => toggleMenu(e)} />
+        </div>
+        <div className="center nav-col ta-center">
+          {title || 'Welcome'}
+        </div>
+        <div className="right nav-col ta-right">
+          <Link to="/user/signin" className="link">
+            <i className="fas fa-user" />
+          </Link>
+        </div>
       </div>
-    </div>
+      <div className="menu-content d-none">
+        <Link to="/classrooms/" className="link">Classrooms</Link>
+        <Link to="/students/" className="link">Curriculum</Link>
+      </div>
+    </>
   );
 };
 
