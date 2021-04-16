a7c057fe424c79c79b2f07ec4d95a3c9c6d561fe
Zappat0n <albarros1973@gmail.com>
2021-04-16 08:57:12 +0200

    Improve Table styling

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -22,36 +22,40 @@ const Table = ({ tableName, id, title }) => {
 
   if (!table) query();
 
-  const filterKeys = (array) => array.filter((key) => !['id', 'created_at', 'updated_at'].includes(key));
+  const filterKeys = (array) => array.filter((key) => (!['id', 'created_at', 'updated_at'].includes(key) && !key.endsWith('_id')));
 
   const redirectTo = (rowId) => {
     if (!id) history.push(`/${tableName}${rowId ? `/${rowId}/` : ''}`);
   };
 
+  const formatColumn = (text) => (text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()).replace('_', ' ');
+
   const addData = () => {
     const response = table.data.map((row) => (
       <tr key={row.id} className="row" onClick={() => redirectTo(row.id)}>
-        {filterKeys(Object.keys(row)).map((key) => (<td key={key}>{row[key]}</td>))}
+        {filterKeys(Object.keys(row)).map((key, index) => (<td className={`column${index + 1}`} key={key}>{row[key]}</td>))}
       </tr>
     ));
     return response;
   };
 
   return (
-    <div className="table">
+    <div className="container-table">
       {(table && table.data) ? (
-        <table>
-          <thead className="table-header">
-            <tr>
-              {filterKeys(Object.keys(table.data[0])).map((value) => (
-                <th key={value}>{value}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody className="table-body">
-            {addData()}
-          </tbody>
-        </table>
+        <div className="table">
+          <table>
+            <thead className="table-head">
+              <tr>
+                {filterKeys(Object.keys(table.data[0])).map((value, index) => (
+                  <th className={`column${index + 1}`} key={value}>{formatColumn(value)}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className="table-body js-pscroll">
+              {addData()}
+            </tbody>
+          </table>
+        </div>
       ) : ''}
     </div>
   );
